977452fc26adccbb2c7ca8ee98737bec82e432fb
Palazzesi <nicolas.palazzesi@qkstudio.com>
2020-04-09 00:23:16 -0300

    add React Redux

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React, { PureComponent } from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-//import { Provider } from 'react-redux';
-//import { PersistGate } from 'redux-persist/lib/integration/react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
-//import {store, persistor} from './store';
+import {store, persistor} from './store';
 import Login from './screens/login';
 import Home from './screens/home';
 
@@ -14,16 +14,16 @@ const Stack = createStackNavigator();
 export default class App extends PureComponent{
   render() {
     return (
-      //<Provider store={store}>
-        //<PersistGate persistor={persistor}>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
           <NavigationContainer>
             <Stack.Navigator initialRouteName="Login" headerMode="none">
               <Stack.Screen name="Home" component={Home} />
               <Stack.Screen name="Login" component={Login} />
             </Stack.Navigator>
           </NavigationContainer>
-        //</PersistGate>
-      //</Provider>
+        </PersistGate>
+      </Provider>
     );
   }
 }
